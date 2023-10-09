3705066f5fda8bf45c8a9b5af7d225fa18abf393
MiriamPaternain <miriampaternain@gmail.com>
2023-10-09 15:05:42 +0200

    add styles to nav and header

--- src/components/MainPage/Header/NavBar.js
+++ src/components/MainPage/Header/NavBar.js
@@ -4,7 +4,16 @@ function NavBar({ menuOpen, handleClick }) {
       {menuOpen && (
         <div className='modal'>
           <div className='navBar'>
-            <a href='/contact' onClick={handleClick}>
+            <a href='/contact' onClick={handleClick} className='link'>
+              Landing Page
+            </a>
+            <a href='/contact' onClick={handleClick} className='link'>
+              Products
+            </a>
+            <a href='/contact' onClick={handleClick} className='link'>
+              Shopping cart
+            </a>
+            <a href='/contact' onClick={handleClick} className='link'>
               Contact
             </a>
           </div>
