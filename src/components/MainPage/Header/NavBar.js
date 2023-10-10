ba9f1ec98755fd18f908718cb0290433093fc7b5
MiriamPaternain <miriampaternain@gmail.com>
2023-10-10 12:48:34 +0200

    add subcategories to nav

--- src/components/MainPage/Header/NavBar.js
+++ src/components/MainPage/Header/NavBar.js
@@ -1,6 +1,16 @@
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 function NavBar({ menuOpen, handleClick }) {
+  const [category, setCategory] = useState([]);
+
+  //call to API to obtain categories
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products/categories')
+      .then((response) => response.json())
+      .then((data) => setCategory(data))
+      .catch((error) => console.error('Error fetching categories:', error));
+  }, []);
   return (
     <>
       {menuOpen && (
@@ -12,6 +22,21 @@ function NavBar({ menuOpen, handleClick }) {
             <Link to='/ProductsPage' onClick={handleClick} className='link'>
               AllProducts
             </Link>
+            {category.length > 0 && (
+              <ul className='subcategories'>
+                {category.map((category, index) => (
+                  <li key={index}>
+                    <Link
+                      to={`/products/${category}`}
+                      onClick={handleClick}
+                      className='subcategories_link'>
+                      {category}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            )}
+
             <Link to='/' onClick={handleClick} className='link'>
               Shopping cart
             </Link>
