8cae5ed30e9a60400bced83e4aa8238d200e026a
MiriamPaternain <miriampaternain@gmail.com>
2023-10-10 14:21:10 +0200

    add link to detail to filter

--- src/components/MainPage/FilterProducts.js
+++ src/components/MainPage/FilterProducts.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import getDataFromApi from '../../services/api';
-import NavBar from './Header/NavBar';
+import { Link } from 'react-router-dom';
+import Header from './Header/Header';
 
 function FilterProducts() {
   const { category } = useParams();
@@ -24,22 +25,26 @@ function FilterProducts() {
 
   return (
     <>
-      <NavBar />
+      <Header />
       <h2 className='filterProducts_title'>{category}</h2>
       <ul className='productList_list'>
         {filteredProducts.map((product) => (
           <li key={product.id} className='productList_eachItem'>
-            <img
-              src={product.image}
-              alt={product.title}
-              className='productList_eachItem--img'
-            />
-            <p className='productList_eachItem--title'>{product.title}</p>
-            <p className='productList_eachItem--price'>{product.price} €</p>
-            <p className='productList_eachItem--description'>
-              {product.description}
-            </p>
-            <p className='productList_eachItem--category'>{product.category}</p>
+            <Link to={`/product/${product.id}`} className='link'>
+              <img
+                src={product.image}
+                alt={product.title}
+                className='productList_eachItem--img'
+              />
+              <p className='productList_eachItem--title'>{product.title}</p>
+              <p className='productList_eachItem--price'>{product.price} €</p>
+              <p className='productList_eachItem--description'>
+                {product.description}
+              </p>
+              <p className='productList_eachItem--category'>
+                {product.category}
+              </p>
+            </Link>
           </li>
         ))}
       </ul>
