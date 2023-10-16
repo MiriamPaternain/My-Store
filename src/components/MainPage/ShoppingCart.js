38baf292ff1020a6443942eea8081026bd50b831
MiriamPaternain <miriampaternain@gmail.com>
2023-10-16 12:07:22 +0200

    structure shopping cart page

--- src/components/MainPage/ShoppingCart.js
+++ src/components/MainPage/ShoppingCart.js
@@ -1,6 +1,11 @@
+import BackButton from '../BackButton';
+import Header from './Header/Header';
+
 function ShoppingCart({ boughtItems }) {
   return (
     <>
+      <Header />
+      <BackButton />
       <h1 className='ShoppingCart_title'>Shopping Cart</h1>
       <ul>
         {boughtItems.map((item, index) => (
