7b259622625f02917b3ea0f8d2d32758105c615b
MiriamPaternain <miriampaternain@gmail.com>
2023-10-20 11:45:34 +0200

    add changing words to landing

--- src/components/landing/LandingPage.js
+++ src/components/landing/LandingPage.js
@@ -1,13 +1,27 @@
+import { useEffect, useState } from 'react';
 import AccessButton from './AccessButton';
 import LandingSlider from './LandingSlider';
 
 function LandingPage() {
+  const words = ['Buy.', 'Explore.', 'Discover.', 'Shop.'];
+  const [currentWordIndex, setCurrentWordIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentWordIndex((prevIndex) => (prevIndex + 1) % words.length);
+    }, 1500); // change word every 2sec
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [words.length]);
+
   return (
     <>
       <main className='main'>
         <div className='main_text'>
           <p className='main_text--title'>Click.</p>
-          <p className='main_text--changingWord'>Buy.</p>
+          <p className='main_text--changingWord'>{words[currentWordIndex]}</p>
           <p className='main_text--word'>Enjoy.</p>
         </div>
 
